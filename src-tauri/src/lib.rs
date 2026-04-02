896aef9668e0228459c78fd71be49aec25dcc7af
Megghy <megghy@qq.com>
2026-04-02 16:30:18 +0800

    feat: 更新依赖项，添加对对话框和文件系统插件的支持

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -237,6 +237,8 @@ fn run_app() {
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(
